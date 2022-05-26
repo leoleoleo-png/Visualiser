f02f9a99c55c4893579b9295e0673052b8b9ef0b
leoleoleo-png <dubois.leo@hotmail.fr>
2022-05-26 17:06:35 +0100

    prepare

--- e/speeder.js
+++ e/speeder.js
@@ -12,9 +12,13 @@ var shapes = 2;
 
 function preload() {
 
-  img4 = loadImage('../input/garbage.jpg')
-  img2 = loadImage("flowers5.png");
-  img3 = loadImage("img2.png");
+
+
+  imgChoice = "img2.png"
+  img = loadImage(imgChoice);
+
+
+
 }
 
 
@@ -45,7 +49,16 @@ function setup() {
   limitLabel.position(10, 40);
 
 
+  let inputLabel = createP("Add your own image");
+  inputLabel.parent(controller);
+  inputLabel.position(10, 105);
 
+
+  input = createFileInput(handleFile);
+  input.parent(controller);
+  input.size(180, 30);
+  let col = color(255, 255, 255, 0);
+  input.style('backgroundColor', col);
 
 
 }
@@ -60,9 +73,8 @@ function draw() {
 
   getMicVolume();
 
-  let mix = val * 5 * micVolume;
+  let mix = val  * micVolume;
 
-  let mox = map(micVolume, 0, 0.6, 1, 100);
 
   noStroke();
 
@@ -73,7 +85,7 @@ function draw() {
 
   beginShape();
 
-  var varImg = img2;
+  var varImg = img;
 
 
 
@@ -83,22 +95,8 @@ function draw() {
 
 
 
-    if (shapes == 1) {
-      varImg = img2;
-    } else if (shapes == 2) {
-      varImg = img3;
-    } else if (shapes == 3) {
-      varImg = img4;
-    }
-
     image(varImg, x, y * 2 - windowHeight / 2, 100 * mix, 100 * mix);
 
-    //image(varImg, x, y * 3 - windowHeight, 1000 * mix, 1000 * mix);
-
-
-
-
-    //translate(200,2);
 
 
   }
@@ -108,36 +106,24 @@ function draw() {
 
 
 
-  copy(0, windowHeight / 2, windowWidth, 10, 0, 0, windowWidth, windowHeight);
+  //copy(0, windowHeight / 2, windowWidth, 10, 0, 0, windowWidth, windowHeight);
 
 
 
-  if (millis() >= 300 + timer) {
-
-    if (map(micVolume*val, 0,3,0,10) > 5) {
-
-      shapesRandom();
-      timer = millis();
-    }
-     
-  }
 
 }
 
 
 
-function shapesRandom() {
 
-  if (shapes == 1) {
 
-    shapes = 2;
-  } else if (shapes == 2) {
 
-    shapes = 3;
-  } else if (shapes == 3) {
+function handleFile(file) {
 
-    shapes = 1;
+  if (file.type === 'image') {
+
+    img = loadImage(file.data, '');
+  } else {
+
   }
-
 }
-
